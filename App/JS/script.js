c245f3cb6e54104f18393d7e2b66ac22b9a548c2
NjimsRane <njims2011@gmail.com>
2022-07-14 22:11:24 +0100

    added medai query and hero image

--- App/JS/script.js
+++ App/JS/script.js
@@ -1,9 +1,11 @@
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const navBar = document.querySelector(".navBar");
 const heroImage = document.querySelector(".heroImage");
+const heading = document.querySelector(".heading");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("hamburgerMenuChange");
   navBar.classList.toggle("navBarChange");
   heroImage.classList.toggle("heroImageChange");
+  heading.classList.toggle("headingChange");
 });
